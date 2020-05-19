23bd609da36636fa34fc965fefcc4af449451f8a
nipunnyka <nipunika.co18@nsut.ac.in>
2020-05-19 20:31:42 +0530

    automated update woohoo

--- .custom_cmd.sh
+++ .custom_cmd.sh
@@ -3,3 +3,9 @@ function runflu(){
     source /home/nipunika/.bash_profile 
     flutter doctor
 }
+
+function gitupdate(){
+    git add .
+    read -p "enter the commit message: " msg
+    git commit -m "$msg"
+}
