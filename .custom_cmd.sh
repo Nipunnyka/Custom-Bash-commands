8906070a320e1e4ff2cb4595de4e08c186288d34
nipunnyka <nipunika.co18@nsut.ac.in>
2020-05-20 00:08:32 +0530

    ading build_icons

--- .custom_cmd.sh
+++ .custom_cmd.sh
@@ -9,3 +9,8 @@ function gitupdate(){
     read -p "enter the commit message: " msg
     git commit -m "$msg"
 }
+
+function build_icons(){
+    flutter pub get
+    flutter pub pub run flutter_launcher_icons:main
+}
