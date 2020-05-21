dd67bbdb081cb8ba1d9e1a4c738af6c05e9b5aac
nipunnyka <nipunika.co18@nsut.ac.in>
2020-05-22 03:58:04 +0530

    added set_origin()

--- .custom_cmd.sh
+++ .custom_cmd.sh
@@ -14,3 +14,8 @@ function build_icons(){
     flutter pub get
     flutter pub pub run flutter_launcher_icons:main
 }
+
+function set_origin(){
+    read -p "enter the url of new remote origin: " newurl
+    git remote set-url origin $newurl
+}
